dc2de6eedcfd1eb5f0e64ccd1562786fd3c21d96
Nandanlagad <nandkumarlagad@outlook.com>
2026-06-07 12:04:26 +0000

    Remove Dev and Test folders

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable vpc {
+  type        = string
+  default     = ""
+  description = "main 
+  VPC"
+}
+variable public_subnet {
+  type        = string
+  default     = ""
+  description = "public subnet"
+}
+variable private_subnet {
+  type        = string
+  default     = ""
+  description = "private subnet"
+}
+
