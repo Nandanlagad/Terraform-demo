ff138b562b99e93f62c28b07d31f4efc7e9460f3
Nandanlagad <nandkumarlagad@outlook.com>
2026-06-11 11:38:35 +0000

    updated AMI block

--- variables.tf
+++ variables.tf
@@ -1,26 +1,26 @@
-variable vpc {
+variable "vpc" {
   type        = string
   default     = ""
   description = "main VPC"
 }
 
-variable private_subnet {
+variable "private_subnet" {
   type        = string
   default     = ""
   description = "private subnet"
 }
 
-variable instance_name {
+variable "instance_name" {
   type        = string
   default     = ""
   description = "Name of the EC2 instance"
 }
-variable security_group_name {
+variable "security_group_name" {
   type        = string
   default     = ""
   description = "Name of the security group"
 }
-variable security_group{
+variable "security_group" {
   type        = string
   default     = ""
   description = "Security group for the EC2 instance"
